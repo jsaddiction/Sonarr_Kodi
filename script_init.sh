a4e9a2787bc1e2d20d4d3546d3476e86eca06bee
jsaddiction <04reduramax@gmail.com>
2024-01-05 14:12:25 -0500

    fix script_init.sh url

--- script_init.sh
+++ script_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bash
-curl https://raw.githubusercontent.com/jsaddiction/Sonarr_Kodi/installer/installer.sh | bash
+curl https://raw.githubusercontent.com/jsaddiction/Sonarr_Kodi/main/installer/installer.sh | bash
