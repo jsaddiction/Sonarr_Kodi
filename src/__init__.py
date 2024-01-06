05ca27ab8ced77d8e57efb1aaea90721b2cc8b18
jsaddiction <04reduramax@gmail.com>
2024-01-05 19:45:02 -0500

    remove date-time from console log

--- src/__init__.py
+++ src/__init__.py
@@ -8,8 +8,8 @@ from .config.models import LogCfg
 # Setup Logging
 LOG_FILE_PATH = "/config/logs"
 LOG_FILE_NAME = "Sonarr_Kodi.txt"
-LOG_FMT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-DATE_FMT = ""
+FILE_LOG_FMT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+CONSOLE_LOG_FMT = "%(name)s - %(levelname)s - %(message)s"
 
 
 def config_log(log_cfg: LogCfg) -> None:
@@ -18,9 +18,11 @@ def config_log(log_cfg: LogCfg) -> None:
     fh = RotatingFileHandler(
         filename=Path(LOG_FILE_PATH, LOG_FILE_NAME), mode="a", maxBytes=1_000_000, backupCount=5, encoding="utf-8"
     )
+    fh.setFormatter(FILE_LOG_FMT)
 
     # Establish stderr output
     sh = logging.StreamHandler()
+    sh.setFormatter(CONSOLE_LOG_FMT)
 
     # Build handler list based on config
     handlers = [sh]
@@ -28,4 +30,4 @@ def config_log(log_cfg: LogCfg) -> None:
         handlers.append(fh)
 
     # Write config to logger
-    logging.basicConfig(format=LOG_FMT, datefmt=DATE_FMT, level=log_cfg.level, handlers=handlers)
+    logging.basicConfig(level=log_cfg.level, handlers=handlers)
